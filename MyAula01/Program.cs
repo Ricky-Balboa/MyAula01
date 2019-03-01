cabde7394dd4a36abfc2b268e084d03c4b1be43e
Aulas <ricardo.m.leal@hotmail.com>
2019-03-01 10:10:53 +0000

    Add project files.

---
+++ MyAula01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyAula01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
